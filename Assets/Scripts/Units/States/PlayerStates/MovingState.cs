c698f43d69fccb738e651ed9a130a13ea2b1e35c
ethoe <ryanouttrim@gmail.com>
2022-03-27 18:04:22 -0700

    Continuous movement on mouse button 1

--- Assets/Scripts/Units/States/PlayerStates/MovingState.cs
+++ Assets/Scripts/Units/States/PlayerStates/MovingState.cs
@@ -34,12 +34,18 @@ public class MovingState : BaseState
             moveTarget = GetMouseLocation();
         }
 
+        if (Input.GetKey(player.controls.Move))
+        {
+            moveTarget = GetMouseLocation();
+        }
+
         attack = Input.GetKeyDown(player.controls.AttackMove);
     }
 
     public override void LogicUpdate()
     {
         base.LogicUpdate();
+
         float distance = Vector2.Distance(player.transform.position, moveTarget);
         if (distance <= .1f)
         {
