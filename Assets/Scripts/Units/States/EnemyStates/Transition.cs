85bf53111f89998a6380f3592c61c769d4c59107
ethoe <ryanouttrim@gmail.com>
2022-04-03 14:14:23 -0700

    add random chance catcher

--- Assets/Scripts/Units/States/EnemyStates/Transition.cs
+++ Assets/Scripts/Units/States/EnemyStates/Transition.cs
@@ -1,17 +1,35 @@
 using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class StateWeight
+{
+    public EnemyBaseState State;
+    public float Weight;
+}
 
 [CreateAssetMenu(menuName = "FSM/Transition")]
 public class Transition : ScriptableObject
 {
     public Decision Decision;
-    public EnemyBaseState TrueState;
+    public List<StateWeight> TrueState = new List<StateWeight>();
     public EnemyBaseState FalseState;
 
     public void Execute(BaseStateMachine stateMachine)
     {
-        if (Decision.Decide(stateMachine) && !(TrueState is RemainInState))
-            stateMachine.ChangeState(TrueState);
+        if (Decision.Decide(stateMachine) && TrueState.Count > 0)
+        {
+            WeightedChanceExecutor weightedChanceExecutor = new WeightedChanceExecutor();
+            foreach (var state in TrueState)
+            {
+                weightedChanceExecutor.AddChance(new WeightedChanceParam(() => { stateMachine.ChangeState(state.State); }, state.Weight));
+            }
+            weightedChanceExecutor.Execute();
+        }
         else if (!(FalseState is RemainInState))
+        {
             stateMachine.ChangeState(FalseState);
+        }
     }
 }
