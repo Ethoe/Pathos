aeab271b1e719ff7a8afea1362cf3625f1e54384
ethoe <ryanouttrim@gmail.com>
2022-04-03 18:02:01 -0700

    Scene update

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -218,7 +218,7 @@ public class RoomManager : MonoBehaviour
         switch (currentRoom.type)
         {
             case RoomType.Standard:
-                for (int count = 0; count < 1; count++)
+                for (int count = 0; count < 2; count++)
                 {
                     Vector2 spawnLocation = new Vector2(Random.Range(-6, 6), Random.Range(-6, 6));
                     GameObject enemy = (GameObject)enemies[Random.Range(0, enemies.Length)];
