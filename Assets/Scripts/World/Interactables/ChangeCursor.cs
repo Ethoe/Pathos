39f3936b99c85dc3511ee9e35a3da1dd0445aec1
ethoe <ryanouttrim@gmail.com>
2022-04-13 18:08:58 -0700

    the power to change the cursor

---
+++ Assets/Scripts/World/Interactables/ChangeCursor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCursor : MonoBehaviour
+{
+    public Texture2D cursor;
+    void Awake()
+    {
+        DefaultCur();
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+    public void ChangeCrsr(Texture2D cursorType)
+    {
+        Vector2 hotspot = new Vector2(cursorType.width / 2, cursorType.height / 2);
+        Cursor.SetCursor(cursorType, Vector2.zero, CursorMode.Auto);
+    }
+    public void DefaultCur()
+    {
+        ChangeCrsr(cursor);
+    }
+}
